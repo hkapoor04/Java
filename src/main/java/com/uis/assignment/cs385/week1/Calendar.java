9e61a406d0267a3f9ec5d16466a31764602c94f4
hkapoor04 <h.kapoor.04@gmail.com>
2020-01-23 17:12:27 -0700

    MS Assignments for week 7 to Week 9

--- src/main/java/com/uis/assignment/cs385/week1/Calendar.java
+++ src/main/java/com/uis/assignment/cs385/week1/Calendar.java
@@ -1,42 +1,60 @@
+
+
 package com.uis.assignment.cs385.week1;
 
 public class Calendar {
 
+    //-- variable for Max Events
     private static final int MAXEVENTS = 4;
 
+    //-- variable for Event object
     private Event[] events;
 
+    //-- variable for number of events
     private int numEvents;
 
+    //-- No argument constructor for initializing the variables
     public Calendar() {
         events = new Event[MAXEVENTS];
         numEvents = 0;
     }
 
+    /*
+     * This method adds an event to the Event array
+     *
+     * @param - The Event Object
+     *
+     * @return - boolean - The result of adding Event object to the array
+     */
     public boolean addEvent(Event e) {
 
+        //-- initializing boolean variable to store the result of event addition
         boolean isAddedSuccess = false;
 
-        if (numEvents != MAXEVENTS) {
-
-            if (events[numEvents] == null) {
+        if (numEvents != MAXEVENTS) { /* check for number of events should not equal to max events to avoid ArrayIndexOutOfBound Exception */
+            if (events[numEvents] == null) { /*  adding the object at the end of the array at empty index */
                 events[numEvents] = e;
                 numEvents++;
-                isAddedSuccess = true;
+                isAddedSuccess = true; /* On successful addition, making the boolean variable true */
             } else {
-                for (int i = 0; i <= numEvents; i++) {
+                for (int i = 0; i <= numEvents; i++) { /* Iterating through array to find any empty index and adding the object to that index */
                     if (events[i] == null) {
                         events[i] = e;
-                        isAddedSuccess = true;
+                        isAddedSuccess = true; /* On successful addition, making the boolean variable true */
                     }
                 }
-
             }
-
         }
         return isAddedSuccess;
     }
 
+    /*
+     * This method find an event to the Event array
+     *
+     * @param - The Event Object
+     *
+     * @return - int - The Event array index in case the object is present, -1 otherwise
+     */
     public int findEvent(Event e) {
         int ret = -1;
         for (int i = 0; i < MAXEVENTS; i++) {
@@ -49,6 +67,14 @@ public class Calendar {
         return ret;
     }
 
+    /*
+     * This method removes an event to the Event array
+     *
+     * @param - The Event Object
+     *
+     * @return - boolean - The result of removing the object from the array
+     */
+
     public boolean removeEvent(Event e) {
 
         int index = findEvent(e);
@@ -56,16 +82,20 @@ public class Calendar {
             return false;
         } else {
             this.events[index] = null;
-            numEvents--;
+            numEvents--; /* decrementing the number of events on removal of an object from the array */
             return true;
         }
 
     }
 
+    /*
+     * This method prints the contents of Event array
+     *
+     * @return - void
+     */
     public void dump() {
         for (int i = 0; i < MAXEVENTS; i++) {
-
-            if (this.events[i] != null) {
+            if (this.events[i] != null) { /* Printing only the non null elements of the array */
                 System.out.println(this.events[i].toString());
             }
 
