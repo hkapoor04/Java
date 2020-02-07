0ba7e071cc138554da44b76005fad1511cee0aeb
hkapoor04 <h.kapoor.04@gmail.com>
2020-02-06 22:07:28 -0700

    MS Assignments for CS 385

---
+++ src/main/java/com/uis/assignment/cs385/week2/Driver.java
@@ -0,0 +1,22 @@
+package com.uis.assignment.cs385.week2;
+
+
+import javax.swing.*;
+import java.io.*;
+
+/**
+ * Driver class for DirectoryLister assignment.
+ * This class simply instantiates the classes needed to run the program, and
+ * executes the program.
+ */
+public class Driver
+{
+
+    public static void main(String[] args)
+    {
+        GUI gui = new GUI();
+        DirectoryLister dl = new DirectoryLister(gui);
+        gui.registerModel(dl);
+    }
+
+}
