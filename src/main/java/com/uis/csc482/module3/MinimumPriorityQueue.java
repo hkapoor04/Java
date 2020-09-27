bf7874afd8b23577197ed42145f92105d6d1d342
harshitkapoor <h.kapoor.04@gmail.com>
2020-09-27 00:50:13 -0700

    Initial commit

--- src/main/java/com/uis/csc482/module3/MinimumPriorityQueue.java
+++ src/main/java/com/uis/csc482/module3/MinimumPriorityQueue.java
@@ -1,5 +1,7 @@
 package com.uis.csc482.module3;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class MinimumPriorityQueue {
@@ -10,37 +12,39 @@ public class MinimumPriorityQueue {
 
     private int maximumSize;
 
-    public MinimumPriorityQueue(int maximumSize)
-    {
+    private Map<Integer, Integer> heapMap;
+
+    public MinimumPriorityQueue(int maximumSize) {
         this.maximumSize = maximumSize;
         this.heapSize = 0;
         heap = new int[this.maximumSize + 1];
         heap[0] = Integer.MIN_VALUE;
+        heapMap = new HashMap<>();
     }
 
 
     /**
-     *  This will check if the heap is empty or not
+     * This will check if the heap is empty or not
      */
-    public boolean isEmpty(){
-        return heapSize==0;
+    public boolean isEmpty() {
+        return heapSize == 0;
     }
 
     /**
-     *  This will check if the heap is full or not
+     * This will check if the heap is full or not
      */
-    public boolean isFull(){
+    public boolean isFull() {
         return heapSize == heap.length;
     }
 
 
-
     /**
      * Creates a priority queue
+     *
      * @param n
      * @return MinimumPriorityQueue
      */
-    public static MinimumPriorityQueue startHeap(int n){
+    public static MinimumPriorityQueue startHeap(int n) {
 
         return new MinimumPriorityQueue(n);
     }
@@ -49,8 +53,8 @@ public class MinimumPriorityQueue {
      * Function to get right child of a node of a tree
      */
     public int getRightChildIndex(int index) {
-        if(index >= 0)
-            return (2*index)+1;
+        if (index >= 0)
+            return (2 * index) + 1;
         return -1;
     }
 
@@ -58,8 +62,8 @@ public class MinimumPriorityQueue {
      * Function to get left child of a node of a tree
      */
     public int getLeftChildIndex(int index) {
-        if(index >= 0)
-            return 2*index;
+        if (index >= 0)
+            return 2 * index;
         return -1;
     }
 
@@ -67,16 +71,15 @@ public class MinimumPriorityQueue {
      * Function to get parent of a node of a tree
      */
     public int getParent(int index) {
-        if(index >= 1)
-            return index/2;
+        if (index >= 1)
+            return index / 2;
         return -1;
     }
 
     /**
-     * Function to swap two node values of heap
+     * Function to swap two nodes' values of heap
      */
-    public void swap(int firstNodeValue, int secondNodeValue)
-    {
+    public void swap(int firstNodeValue, int secondNodeValue) {
         int tmp;
         tmp = heap[firstNodeValue];
         heap[firstNodeValue] = heap[secondNodeValue];
@@ -86,11 +89,11 @@ public class MinimumPriorityQueue {
     /**
      * Function to heapify up
      */
-    public void heapify_up(int index){
+    public void heapify_up(int index) {
 
-        if(index > 1){
+        if (index > 1) {
             int parentIndex = getParent(index);
-            if(heap[index] < heap[parentIndex]){
+            if (heap[index] < heap[parentIndex]) {
                 swap(index, parentIndex);
 
                 heapify_up(parentIndex);
@@ -101,27 +104,27 @@ public class MinimumPriorityQueue {
     /**
      * Function to heapify down
      */
-    public void heapify_down(int index){
+    public void heapify_down(int index) {
 
-        int leftChildIndex =  getLeftChildIndex(index);
+        int leftChildIndex = getLeftChildIndex(index);
 
         int rightChildIndex = getRightChildIndex(index);
 
         int smallest = 0;
 
-        if((leftChildIndex < heap.length ) && heap[leftChildIndex] < heap[index]){
-            smallest =  leftChildIndex;
+        if ((leftChildIndex < heap.length) && heap[leftChildIndex] < heap[index]) {
+            smallest = leftChildIndex;
 
         } else {
             smallest = index;
         }
 
-        if((rightChildIndex < heap.length) && heap[rightChildIndex] < heap[smallest]){
-            smallest =  rightChildIndex;
+        if ((rightChildIndex < heap.length) && heap[rightChildIndex] < heap[smallest]) {
+            smallest = rightChildIndex;
 
         }
 
-        if(smallest != index){
+        if (smallest != index) {
             swap(index, smallest);
             heapify_down(smallest);
         }
@@ -131,7 +134,7 @@ public class MinimumPriorityQueue {
     /**
      * Function to find the least element in the heap
      */
-    public int findMin(){
+    public int findMin() {
 
         return heap[1];
     }
@@ -139,56 +142,113 @@ public class MinimumPriorityQueue {
     /**
      * Function to identify the element with the minimum key value
      */
-    public int extractMin()
-    {
+    public int extractMin() {
+        //-- Extracting the first element as it is the minimum
         int element = heap[1];
+
+        //-- Deleting the first element from the heap by moving the item in the last array position to first
         heap[1] = heap[heapSize--];
+
+        //-- calling heapify down to readjust the entire heap
         heapify_down(1);
+
+        //-- The deleted element is returned
         return element;
     }
 
 
     /**
-     *  This will delete the element at the position index
+     * This will delete the element at the position index
+     *
+     * @param index - The index of the entry to be deleted
      */
-    public int delete(int index){
-        if(isEmpty())
+    public int delete(int index) {
+        if (isEmpty())
             throw new NoSuchElementException("Heap is empty, no element to delete");
 
+        //-- Finding the element at the particular index in the heap
         int element = heap[index];
-        heap[index] = heap[heapSize -1];
+
+        //-- Deleting the element from the heap by moving the item in the last array position to index
+        heap[index] = heap[heapSize - 1];
+
+        //-- Reducing the heap size upon deletion of an entry
         heapSize--;
+
+        //-- calling heapify down to readjust the entire heap
         heapify_down(index);
+
+        //-- The deleted element is returned
         return element;
     }
 
 
     /**
-     *  This will insert new element in to heap
+     * This will insert new element in to heap
+     *
+     * @param element - The element to be inserted in heap
      */
-    public void insert(int element){
-        if(isFull())
+    public void insert(int element) {
+        if (isFull())
             throw new NoSuchElementException("Heap is full, cannot insert new element");
+
+        //-- Inserting new element at index 0
         heap[0] = element;
+
+        //-- calling heapify down to readjust the entire heap
         heapify_down(0);
+
+        //-- Incrementing the heap size with each addition
         heapSize++;
-
-        //heapSize++;
-
-        //heap[heapSize - 1] = element;
-
-        //heapify_up(heapSize - 1);
     }
 
 
     /**
-     *  This method used to print all element of the heap
-     *
+     * This method used to print all element of the heap
      */
-    public void printHeap()
-    {
+    public void printHeap() {
         for (int i = 1; i < maximumSize; i++)
-            System.out.println(heap[i] +" ");
+            System.out.println(heap[i] + " ");
+    }
+
+
+    /**
+     * This method adds all element of the heap in a map
+     */
+    public Map<Integer, Integer> populatePosition() {
+        for (int i = 1; i < maximumSize; i++) {
+            heapMap.put(heap[i], i);
+        }
+        return heapMap;
+    }
+
+    /**
+     * This method deletes a particular item from heap
+     *
+     * @param item - The item to be deleted
+     */
+    public int deleteItem(int item) {
+        return delete(item);
+    }
+
+    /**
+     * This method changes a particular item in heap
+     *
+     * @param item - The item to be changed
+     * @param newValue - The new value of the item
+     */
+
+    public void changeKey(int item, int newValue){
+
+        for(int i = 1; i< maximumSize; i++){
+            if(heap[i] != item){
+                throw new NoSuchElementException("The item {} does not exist in heap" + item);
+            } else{
+                heap[i] = newValue;
+                heapify_down(i);
+            }
+        }
+
     }
 
 }
--- src/main/java/com/uis/csc482/module3/PriorityQueueTest.java
+++ src/main/java/com/uis/csc482/module3/PriorityQueueTest.java
@@ -3,21 +3,35 @@ package com.uis.csc482.module3;
 public class PriorityQueueTest {
 
     public static void main(String[] args){
-        MinimumPriorityQueue maxHeap = new MinimumPriorityQueue(10);
-        maxHeap.insert(10);
-        maxHeap.insert(4);
-        maxHeap.insert(9);
-        maxHeap.insert(1);
-        maxHeap.insert(7);
-        maxHeap.insert(5);
-        maxHeap.insert(3);
-        maxHeap.insert(2);
-        maxHeap.insert(6);
-        maxHeap.insert(8);
+        MinimumPriorityQueue minimumPriorityQueue = new MinimumPriorityQueue(10);
+        minimumPriorityQueue.insert(10);
+        minimumPriorityQueue.insert(4);
+        minimumPriorityQueue.insert(9);
+        minimumPriorityQueue.insert(1);
+        minimumPriorityQueue.insert(7);
+        minimumPriorityQueue.insert(5);
+        minimumPriorityQueue.insert(3);
+        minimumPriorityQueue.insert(2);
+        minimumPriorityQueue.insert(6);
+        minimumPriorityQueue.insert(8);
 
-        maxHeap.printHeap();
-        //maxHeap.delete(5);
+
+        //System.out.println("Minimum " + maxHeap.extractMin());
+        //maxHeap.delete(0);
+
+        minimumPriorityQueue.populatePosition();
+        minimumPriorityQueue.printHeap();
+        System.out.println("MinimumNow " + minimumPriorityQueue.extractMin());
         //maxHeap.printHeap();
+        //maxHeap.delete(0);
 
+        System.out.println("Minimum Again " + minimumPriorityQueue.extractMin());
+
+        System.out.println("Minimum Again1 " + minimumPriorityQueue.extractMin());
+
+        System.out.println("Minimum Again2 " + minimumPriorityQueue.extractMin());
+
+        System.out.println("Minimum Again3 " + minimumPriorityQueue.extractMin());
+        //maxHeap.printHeap();
     }
 }
